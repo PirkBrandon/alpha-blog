02ddfd39c69f1cf1e11bb9b86e11de08b5711d0b
Brandon Pirk <b.pirk97@htl-ottakring.ac.at>
2017-10-06 14:01:35 +0200

    Add Delete

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -23,8 +23,16 @@ class ArticlesController < ApplicationController
        #redirect_to article_path(@article)
    end
 
+   
    def show
        @article = Article.find(params[:id])
+   end
+
+   def destroy
+       @article = Article.find(params[:id])
+       @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+      redirect_to articles_path
    end
 
    def edit
